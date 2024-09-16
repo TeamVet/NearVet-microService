8df189da6d557c09d213ac2f4b75851aa84cd14a
facuzero <facuzero_95@hotmail.com>
2024-09-16 20:59:50 -0300

    microService Stripe

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.TCP,
+    options: { port: 3001 },
+  });
+  await app.listen();
+}
+bootstrap();
---
+++ src/modules/payments/payments.controller.ts
@@ -0,0 +1,59 @@
+import { Controller, Get, Post } from '@nestjs/common';
+//import { ApiBody, ApiTags } from '@nestjs/swagger';
+//import { MercadopagoService } from './mercado-pago/mercado-pago.service';
+//import { MercadoPagoDTO } from './mercado-pago/mercado-pago.dto';
+import { StripeService } from './stripe/stripe.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+
+//@ApiTags('payments')
+@Controller('payments')
+export class PaymentsController {
+  constructor(
+    //private readonly mercadopagoService: MercadopagoService,
+    private readonly stripeService: StripeService,
+  ) {}
+  /* 
+  @Post('/mercado-pago')
+  @ApiBody({
+    description: 'Se deben agregar los campos de titulo, cantidad y precio',
+    required: true,
+    type: MercadoPagoDTO,
+  })
+  async createPayment(@Body() body: MercadoPagoDTO) {
+    const { title, quantity, price } = body;
+
+    try {
+      const preference = await this.mercadopagoService.createPreference(title, quantity, price);
+      return { preferenceId: preference.id };
+    } catch (error) {
+      console.error('Error en la creación de la preferencia:', error);
+      throw error;
+    }
+  } */
+  @Get('/stripe/products')
+  async getProducts() {
+    return await this.stripeService.getProducts();
+  }
+
+  @Get('/stripe/customers')
+  async getCustomers() {
+    return await this.stripeService.getCustomers();
+  }
+  /* 
+  @Post('/stripe/create-payment-intent')
+  async createPaymentIntent(@Body('amount') amount: number) {
+    const paymentIntent = await this.stripeService.createPaymentIntent(amount, 'usd');
+    return {
+      clientSecret: paymentIntent.client_secret,
+    };
+  } */
+  @MessagePattern('pattern-name')
+  @Post('/stripe/create-checkout-session')
+  /* @ApiBody({
+    description: 'Solo se ingresa el ID del producto cargado en el dashboard/products de stripe',
+    required: true,
+  }) */
+  async createCheckoutSession(@Payload() priceId: string) {
+    return await this.stripeService.createCheckoutSessionService(priceId);
+  }
+}
