a9b43dfac3efd990dfc05618bdf4adf893151adb
facuzero <facuzero_95@hotmail.com>
2024-09-17 14:36:34 -0300

    agrego logs a nestJS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
+  });
   const port = process.env.PORT || 3001; // Render asignará el puerto desde el entorno
 
   await app.listen(port, '0.0.0.0'); // Asegúrate de usar '0.0.0.0' para ser accesible externamente
