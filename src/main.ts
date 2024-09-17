79750c28de00e500a2ba4eb25281982cd6aa6869
facuzero <facuzero_95@hotmail.com>
2024-09-17 00:58:49 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,23 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
-  const port = process.env.PORT || 3001;
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-    transport: Transport.TCP,
+  const port = process.env.PORT || 3001; // Usar el puerto dinámico de Render
+  const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
     options: {
       port: Number(port),
-      host: '52.41.36.82' || '54.191.253.12' || '44.226.122.3',
+      host: '0.0.0.0',
     },
   });
-  await app.listen();
+
+  await app.startAllMicroservices();
+  await app.listen(port);
+  console.log(`Microservice listening on port: ${port}`);
 }
+
 bootstrap();
