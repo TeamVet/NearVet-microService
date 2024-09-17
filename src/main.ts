1a4e56dd8e148241745d5d1b6609d69e66f9599d
facuzero <facuzero_95@hotmail.com>
2024-09-17 16:37:25 -0300

    abro puerto 3002

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,13 @@ async function bootstrap() {
       host: '0.0.0.0', // Asegura que el servicio sea accesible
     },
   });
+  const httpApp = await NestFactory.create(AppModule);
+  await httpApp.listen(3002); // Puerto HTTP
   await app.listen();
   console.log('Microservice is running on port 3001');
 }
-
+/* 
+==> Port scan timeout reached, no open HTTP ports detected. 
+If you don't need to receive public HTTP traffic, create a private service instead.
+ */
 bootstrap();
