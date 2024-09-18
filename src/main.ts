63478be85f9b8554b892738424063871069b8c4a
facuzero <facuzero_95@hotmail.com>
2024-09-17 22:49:14 -0300

    configuro solo HTTP server

--- src/main.ts
+++ src/main.ts
@@ -6,19 +6,9 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-    transport: Transport.TCP, // Transport en TCP
-    options: {
-      port: 3001, // Asegúrate de que el puerto coincida con el configurado en el monolito
-      host: '0.0.0.0', // Asegura que el servicio sea accesible
-    },
-  });
-  const httpApp = await NestFactory.create(AppModule, {
-    logger: ['error'],
-  });
-  await httpApp.listen(3002); // Puerto HTTP
-  await app.listen();
-  console.log('Microservice is running on port 3001');
+  const httpApp = await NestFactory.create(AppModule);
+  await httpApp.listen(3000); // Puerto HTTP
+  console.log('HTTP server is running on port 3000');
 }
 /* 
 ==> Port scan timeout reached, no open HTTP ports detected. 
